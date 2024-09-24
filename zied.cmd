19269504ff4632921bd65d6fea00a681982c86f3
Zied <zinzied@gmail.com>
2024-09-24 15:22:10 +0100

    Update zied.cmd

--- zied.cmd
+++ zied.cmd
@@ -391,7 +391,7 @@ choice /C:123450 /N
 set _erl=%errorlevel%
 
 if %_erl%==6 exit /b
-if %_erl%==5 start https://github.com/zinzied & goto MainMenu
+if %_erl%==5 start https://github.com/zinzied/IDM-Freezer & goto MainMenu
 if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
 if %_erl%==3 goto _reset
 if %_erl%==2 (set frz=1&goto :_activate)
