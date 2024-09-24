65527cd367bc6d3428e2c23686b3301638679b2d
Zied <zinzied@gmail.com>
2024-09-24 15:17:20 +0100

    Update IDM Freezer.py

--- IDM Freezer.py
+++ IDM Freezer.py
@@ -15,17 +15,16 @@ def main():
      ██║██║  ██║██║╚██╔╝██║    ██╔══╝  ██╔══██╗██╔══╝  ██╔══╝   ███╔╝  ██╔══╝  ██╔══██╗
      ██║██████╔╝██║ ╚═╝ ██║    ██║     ██║  ██║███████╗███████╗███████╗███████╗██║  ██║
      ╚═╝╚═════╝ ╚═╝     ╚═╝    ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝
-                                                            BY ZIEDEV 2024  """)
-    print("\033[1;32m1. Batch file\033[0m")
-    print("\033[1;32m2. PowerShell script\033[0m")
+                                                            BY ZIEDEV 2024  
+        """)
     
-    choice = input("ENTER YOUR CHOICE (1 OR 2): ")
+    print("\033[1;32m1. Batch file\033[0m")
+
+    
+    choice = input("ENTER YOUR CHOICE (1): ")
     
     if choice == '1':
         run_batch_file()
-    elif choice == '2':
-        run_powershell_script()
-    else:
         print("Invalid choice. Please enter 1 or 2.")
 
 def run_batch_file():
@@ -33,12 +32,6 @@ def run_batch_file():
         subprocess.run(["cmd.exe", "/c", "zied.cmd"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running the batch file: {e}")
-
-def run_powershell_script():
-    try:
-        subprocess.run(["powershell.exe", "-File", "zied.ps1"], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while running the PowerShell script: {e}")
 
 if __name__ == "__main__":
     main()
